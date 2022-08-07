b7ae371aa6c1202e4c3384f6c9bddc79ad9c2fdb
nguyentt-khtn <ttnguyenkhtn@gmail.com>
2022-08-07 22:03:44 +0700

    update ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { Route, Router,Switch } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import ChatRoom from './pages/ChatRoom/ChatRoom';
 import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <Router >
-      <Switch >
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/' component={ChatRoom} />
-      </Switch>
-    </Router>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ChatRoom />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- src/pages/ChatRoom/ChatRoom.js
+++ src/pages/ChatRoom/ChatRoom.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Avatar, Button, Stack, Typography } from '@mui/material';
+import { Logout } from '@mui/icons-material';
 
 export default function ChatRoom() {
+
+  const renderListUser = (number) => {
+    const userList = [];
+    for (let i = 0; i < number; i++) {
+      userList.push(<Stack direction='row' spacing={2} mt={1}>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <Typography sx={{ display: 'flex', alignItems: 'center' }}>asdkjasd</Typography>
+      </Stack>)
+    }
+    return userList
+  }
+
   return (
-    <div>
-      ChatRoom
-    </div>
+    <Box sx={{ flexGrow: 1, maxHeight:'100vh'}}>
+      <Grid container>
+        <Grid item xs={2} p={2} sx={{ backgroundColor: 'rgba(0,0,0,0.2)' }} >
+
+          {/*Start User Information */}
+          <Stack direction="row" justifyContent='space-between' spacing={2}>
+            <Stack direction="row" spacing={2}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+              <Typography sx={{ display: 'flex', alignItems: 'center' }}>asdkjasd</Typography>
+            </Stack>
+            <Button sx={{ float: 'right' }} variant="outlined" color="error" ><Logout /></Button>
+          </Stack>
+          {/*End User Information */}
+
+          <hr />
+
+          {/* Start List User Contact */}
+          <Box mt={5} sx={{overflowY:'clip'}}>
+            {renderListUser(20)}
+          </Box>
+
+          {/* End List User Contact */}
+
+        </Grid>
+        <Grid item xs={10} >
+          xs=4
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
