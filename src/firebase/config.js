eb827ce4ff2467c7a311c5fb6e2c174ce9e47f98
nguyentt-khtn <ttnguyenkhtn@gmail.com>
2022-08-09 16:34:43 +0700

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,22 +1,6 @@
-import firebase from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {
-  GoogleAuthProvider,
-  getAuth,
-  signInWithPopup,
-  ignInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signOut,
-} from "firebase/auth";
-import {
-  getFirestore,
-  query,
-  getDocs,
-  collection,
-  where,
-  addDoc,
-} from "firebase/firestore";
+import {initializeApp} from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -29,13 +13,11 @@ const firebaseConfig = {
   storageBucket: "chat-app-6565a.appspot.com",
   messagingSenderId: "883321942086",
   appId: "1:883321942086:web:bf717ea778b23e48bb1a47",
-  measurementId: "G-PNBSBCH3JZ",
+  measurementId: "G-PNBSBCH3JZ"
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
-export {auth, db}
-export default firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp)
+
+export {auth}
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { FacebookAuthProvider, signInWithPopup } from 'firebase/auth';
+
+const fbProvider = new FacebookAuthProvider()
 
 export default function Login() {
+
+  const handleFbLogin = () => {
+    signInWithPopup(fbProvider)
+  }
   return (
-    <div>
-      
-    </div>
+    <Stack spacing={2} direction="row">
+      <Button variant="contained" onClick={handleFbLogin}>Sign in with Facebook</Button>
+    </Stack>
   )
 }
